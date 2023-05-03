01e4a5ff121d70d2df41c7aea7282b20708dd542
Abundancia <abundancia@MacBook-Air-de-Abundancia.local>
2023-05-03 11:58:36 +0200

    calculer la superficie a l'aide d'une requete

--- jour02/job05.py
+++ jour02/job05.py
@@ -1,2 +1,21 @@
 """À l’aide d’un requête calculer la superficie de l’ensemble des étages et afficher “La superficie de La Plateforme est de X m2”, X étant le résultat de la requête."""
 
+#SLECT SUM(superficie) FROM etage
+import mysql.connector
+
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="velvet",
+    database="laplateforme",
+)
+
+print(db)
+
+cursor = db.cursor()
+
+cursor.execute("SELECT SUM(superficie) FROM etage;")
+resultats = cursor.fetchall()
+for resultat in resultats:
+    print(resultat)
